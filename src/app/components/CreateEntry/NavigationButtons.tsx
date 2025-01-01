bbbecb1528530b4f6ba7588bf1aee602156571c5
ninjonas <jonas@ezed.ca>
2024-12-31 16:15:02 -0800

    [feat] enabling deploy feature to github pages

--- src/app/components/CreateEntry/NavigationButtons.tsx
+++ src/app/components/CreateEntry/NavigationButtons.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React from "react";
 
 interface NavigationButtonsProps {
   currentStep: number;
@@ -8,40 +8,26 @@ interface NavigationButtonsProps {
   handleSubmit: () => void;
 }
 
-const NavigationButtons: React.FC<NavigationButtonsProps> = ({
-  currentStep,
-  steps,
-  handlePreviousStep,
-  handleNextStep,
-  handleSubmit,
-}) => {
+const NavigationButtons: React.FC<NavigationButtonsProps> = ({ currentStep, steps, handlePreviousStep, handleNextStep, handleSubmit }) => {
+  const isProd = process.env.NEXT_PUBLIC_ENV === "production";
+
+  console.log("Environment: ", process.env.NEXT_PUBLIC_ENV);
+
   return (
     <div className="flex justify-between mt-6">
       {currentStep > 1 && (
-        <button
-          type="button"
-          className="px-4 py-2 bg-gray-200 rounded-md"
-          onClick={handlePreviousStep}
-        >
+        <button type="button" className="px-4 py-2 bg-gray-200 rounded-md" onClick={handlePreviousStep}>
           Previous
         </button>
       )}
       <div className="flex-grow"></div>
       {currentStep < steps && (
-        <button
-          type="button"
-          className="px-4 py-2 bg-blue-600 text-white rounded-md"
-          onClick={handleNextStep}
-        >
+        <button type="button" className="px-4 py-2 bg-blue-600 text-white rounded-md" onClick={handleNextStep}>
           Next
         </button>
       )}
-      {currentStep === steps && (
-        <button
-          type="button"
-          className="px-4 py-2 bg-green-600 text-white rounded-md"
-          onClick={handleSubmit}
-        >
+      {currentStep === steps && !isProd && (
+        <button type="button" className="px-4 py-2 bg-green-600 text-white rounded-md" onClick={handleSubmit}>
           Confirm & Submit
         </button>
       )}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,6 +5,8 @@ import DisplayEntries from "./components/DisplayEntries/index";
 
 export default function Home() {
   const [showCreateEntry, setShowCreateEntry] = useState(true);
+  const isProd = process.env.NEXT_PUBLIC_ENV === "production";
+  console.log("Environment: ", process.env.NEXT_PUBLIC_ENV);
 
   const toggleView = () => {
     setShowCreateEntry(!showCreateEntry);
@@ -15,14 +17,16 @@ export default function Home() {
       <div className="max-w-2xl mx-auto bg-white p-3 rounded-md shadow">
         <div className="flex justify-between items-center mb-4">
           <h1 className="text-2xl font-bold">The Cognitive Triangle</h1>
-          <button onClick={toggleView} className="bg-blue-500 text-white px-4 py-2 rounded-md shadow">
-            {showCreateEntry ? "View Entries" : "Create New Entry"}
-          </button>
+          {!isProd && (
+            <button onClick={toggleView} className="bg-blue-500 text-white px-4 py-2 rounded-md shadow">
+              {showCreateEntry ? "View Entries" : "Create New Entry"}
+            </button>
+          )}
         </div>
         <p className="mb-4">
           The cognitive triangle illustrates how thoughts, emotions, and behaviors affect one another. This idea forms the basis of cognitive behavior therapy (CBT).
         </p>
-        {showCreateEntry ? <CreateEntry /> : <DisplayEntries />}
+        {showCreateEntry ? <CreateEntry /> : !isProd && <DisplayEntries />}
       </div>
     </main>
   );
