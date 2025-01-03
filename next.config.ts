254660eaf5ff2ff7ca39683c4079b8bda3f642b9
ninjonas <jonas@ezed.ca>
2025-01-03 02:09:11 -0800

    [chore] removing static export logic

--- next.config.ts
+++ next.config.ts
@@ -1,12 +1,12 @@
 import type { NextConfig } from "next";
 
 //used to deploy to github pages
-const isProd = process.env.NODE_ENV === "production";
+// const isProd = process.env.NODE_ENV === "production";
 
 const nextConfig: NextConfig = {
-  output: isProd ? "standalone" : "export",
-  assetPrefix: isProd ? "/cbt-triangle/" : "",
-  basePath: isProd ? "/cbt-triangle" : "",
+  // output: isProd ? "standalone" : "export",
+  // assetPrefix: isProd ? "/cbt-triangle/" : "",
+  // basePath: isProd ? "/cbt-triangle" : "",
   /* config options here */
 };
 
